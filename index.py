e186de98443cd329aaa1641194e272682d4b80bb
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2019-12-15 13:09:44 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+#the division calculator!
+num1 = input("please enter dividend: ")
+num2 = input("please enter divisor: ")
+num1 = float(num1)
+num2 = float(num2)
+quotiant = num1 / num2
+num1 = str(num1)
+num2 = str(num2) 
+quotiant = str(quotiant)
+print(num1 + " / " + num2 + " = " + quotiant)
